8c94d48c818ce6407695710f251237133e2f8a83
Jobair <m.j.khan.bd@gmail.com>
2019-07-07 01:38:36 +0100

    Before adding gift controller

--- JG.FinTechTest.Tests/Controllers/GiftAidControllerTests.cs
+++ JG.FinTechTest.Tests/Controllers/GiftAidControllerTests.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using JG.FinTechTest.Controllers;
+using JG.FinTechTest.GiftAid;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 
@@ -10,29 +11,42 @@ namespace JG.FinTechTest.Tests.Controllers
     [TestFixture]
     public class GiftAidControllerTests
     {
+        GiftAidController _sut;
         [SetUp]
         public void Setup()
         {
+            _sut = new GiftAidController();
         }
 
         [Test]
-        public void Test_Should_return_OK()
+        public void Ping_Should_return_OK()
         {
-            var sut = new GiftAidController();
-            var actual = sut.Test();
+            var actual = _sut.Test();
             Assert.That(actual, Is.TypeOf<OkObjectResult>());
         }
 
         [Test]
         public async Task GET_Should_return_OK()
         {
-            var sut = new GiftAidController();
-            decimal donationAmount = 100M;
+            const decimal donationAmount = 100M;
 
-            var actual = await sut.GetGiftAid(donationAmount, CancellationToken.None);
+            var actual = await _sut.GetGiftAid(donationAmount, CancellationToken.None);
 
-            Assert.That(actual, Is.Not.Null);
             Assert.That(actual?.Result, Is.InstanceOf<OkObjectResult>());
+        }
+
+        [Test]
+        public async Task GET_Should_return_CorrectValue()
+        {
+            const decimal donationAmount = 100M;
+            const decimal taxRate = 20;
+            decimal expected = donationAmount.GiftAidCalculation(taxRate).Round2();
+
+            var act = await _sut.GetGiftAid(donationAmount, CancellationToken.None);
+
+
+            dynamic actual = act?.Result;
+            Assert.That(actual.Value, Is.EqualTo(expected));
         }
     }
 }
