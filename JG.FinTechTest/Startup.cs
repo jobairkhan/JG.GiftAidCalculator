f3292952ecec30b90fcb2ada08d7c3d40a889241
Jobair <m.j.khan.bd@gmail.com>
2019-07-07 01:54:14 +0100

    fix failing test

--- JG.FinTechTest/Startup.cs
+++ JG.FinTechTest/Startup.cs
@@ -21,7 +21,7 @@ namespace JG.FinTechTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.TryAddTransient<IStoreTaxRate, TaxRateStorage>();
+            services.TryAddTransient<IStoreTaxRate, TaxRateStorage>();
             services.TryAddTransient<GiftAidCalculator>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
